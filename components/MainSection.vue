ae426ce73a199f19c2108e64afe1e5a16e8a3825
hafidnurkholis <itsmaehere@gmail.com>
2022-12-19 15:37:00 +0700

    main section initialization

---
+++ components/MainSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="h-full flex flex-col border-y-0" :class="twitterBorder">
+        <div class="sticky top-0 p-4 bg-light-100 backdrop-blur-md dark:bg-dim-900/70">
+            <h2 class="text-xl font-bold text-gray-900 backdrop-blur-md dark:text-light-100">{{ title }}</h2>
+        </div>
+        <div v-if="isLoading" class="flex-1 grid place-items-center">
+            <UISpinner />
+        </div>
+        <div v-else class="px-4">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    isLoading: {
+        type: Boolean,
+        required: true
+    }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="h-full">
+        <MainSection title="Home" :isLoading="isLoading">
+            This is our Home page
+        </MainSection>
+    </div>
+</template>
+
+<script setup>
+const isLoading = ref(false)
+</script>
